589b71c3859993762a7a5da5db4f09270aff1d5c
Roopa Madihally <roopabdr@gmail.com>
2021-12-23 11:11:11 +0530

    added about us page content and related css

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,23 @@
+import './Pages.css';
+
 function About() {
-    return (<h2>About</h2>);
+    return (
+        <div className="web-content about">
+            <h2>About Us</h2>
+            <p>An initiative to facilitate moral and cultural values in little minds by connecting them to our roots.
+                These values are the strong pillars which support them to grow up to be confident individuals and lead 
+                a virtuous life.
+            </p>
+            <p>
+            Here <b>kids learn Sarnskrit Shlokas (Mantras) along with Mudras</b>, activities which teach 
+                them moral and cultural values (stories quiz and many more to come). We also conduct <b>one to one</b> Shloka 
+                sessions and group sessions. These sessions are conducted through ZOOM.
+            </p>
+            <p>
+                <b>Age groups 3-5, 6-8, 9-12</b>
+            </p>
+        </div>
+    );
 }
 
 export default About;
